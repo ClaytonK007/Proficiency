a728a18bd1feac9701559dbee021229d2b8a6110
ClaytonK007 <72389807+ClaytonK007@users.noreply.github.com>
2026-07-20 16:52:40 +0200

    Add files via upload

---
+++ to_do_app/app/models.py
@@ -0,0 +1,12 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+
+class Task(Base):
+
+    __tablename__ = "tasks"
+
+    id = Column(Integer,primary_key= True, index= True)
+    title = Column(String, nullable= False, index= True)
+    completed = Column(Boolean, nullable= False, default= True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
