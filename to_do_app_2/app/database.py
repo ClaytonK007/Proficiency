262d46680a07f8b412323fa7b36bcc62671aaa3e
ClaytonK007 <72389807+ClaytonK007@users.noreply.github.com>
2026-07-27 22:30:20 +0200

    Modify database connection string for clarity
    Updated database connection string to include a placeholder for the password.

--- to_do_app_2/app/database.py
+++ to_do_app_2/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password!@localhost:5432/todos_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:(password)@localhost:5432/todos_db" # your PostgreSQL password
 Base = declarative_base()
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
