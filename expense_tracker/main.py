6da10d93139d95f8d98b1ee2e1a6e783813b5b0a
ClaytonK007 <72389807+ClaytonK007@users.noreply.github.com>
2026-07-27 15:19:52 +0200

    updated html and main
    added the functions to the routes in main
    added html base and index

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -1,12 +1,48 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+from datetime import date
+from database import *
+from chart import generate_category_chart
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+init_db()
+
 
 @app.get("/")
+def get_expenses(request: Request):
+    expenses = get_expenses()
+    category_totals = get_category_totals()
+    total_spent = get_total_spent()
+    chart_image = generate_category_chart(category_totals)
+
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {
+            "expenses": expenses,
+            "category_totals": category_totals,
+            "total_spent": total_spent,
+            "chart_image": chart_image,
+            "categories": CATEGORIES,
+            "today": date.today().isoformat()
+        }
+    )
+
 
 @app.post("/add")
+def add_expenses(
+    description: str = Form(...),
+    category: str = Form(...),
+    amount: float = Form(...),
+    expense_date: str = Form(...)
+):
+    add_expense(description.strip(), category, amount, expense_date)
+    return RedirectResponse(url="/", status_code=303)
 
-@app.post("delete/{id}")
+
+@app.post("delete/{expense_id}")
+def delete_expense(expense_id: int):
+    delete_expense(expense_id)
+    return RedirectResponse(url="/", status_code=303)
